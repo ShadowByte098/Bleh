9f2ace8db58dc9ac734ffc6d2a3f4527e682609c
Grandma96523 <131499218+ShadowByte098@users.noreply.github.com>
2025-02-08 14:31:28 -1000

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-// pages/index.js
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>If you can see this text, then index.js is working.</p>
-    </div>
-  );
-}
