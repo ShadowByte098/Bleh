bad3250395c2a9d0872d50e83f3d26a12ef82587
Grandma96523 <131499218+ShadowByte098@users.noreply.github.com>
2025-02-08 14:18:10 -1000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>If you can see this text, then index.js is working.</p>
+    </div>
+  );
+}
