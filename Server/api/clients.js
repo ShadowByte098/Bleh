bb724d44962775b630d448c2293d49dde096df23
Grandma96523 <131499218+ShadowByte098@users.noreply.github.com>
2025-02-17 17:40:40 -1000

    Delete Server directory

--- Server/api/clients.js
+++
@@ -1,8 +0,0 @@
-// api/clients.js
-
-// Retrieve the global clients array.
-let clients = global.__clients || [];
-
-export default function handler(req, res) {
-  res.status(200).json({ clients });
-}
